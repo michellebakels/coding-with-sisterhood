3924d8d058b07b26527b7b193770d41283a2d812
Rebecca Bakels <rebeccabakels@gmail.com>
2021-03-29 13:50:58 -0400

    made header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="home-header">
+        <h1 className="h1-header">The Wag Group</h1>
       </header>
+      <footer className="home-footer">
+        <h2 className="title-footer">Contact Wag Group:</h2>
+        <p>
+          Email: wagmeister.beth@gmail.com
+          <br/>
+          Phone: 561-410-8840
+        </p>
+        <div className="link-group">
+          <a
+            href="https://www.linkedin.com/in/bethwagmeister/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="links"
+              src="assets/linkedin.png"
+              alt="Linkedin Logo"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/bethwagmeister/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="links"
+              src="assets/instagram.png"
+              alt="Instagram Logo"
+            />
+          </a>
+          <a
+            href="https://twitter.com/BethWagmeister"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="links"
+              src="assets/twitter.png"
+              alt="Twitter Logo"
+            />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
